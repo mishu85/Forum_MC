53014f507b69af5a39d3617d8682eb8982f70bea
mishu85 <miconstantin@yahoo.com>
2022-10-09 13:43:44 +0300

    enable swagger

--- ForumMCBackend/ForumMCBackend/Program.cs
+++ ForumMCBackend/ForumMCBackend/Program.cs
@@ -14,8 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<MyDbContext>();
 
@@ -36,11 +36,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.environment.isdevelopment())
-//{
-//    app.useswagger();
-//    app.useswaggerui();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UsePathBase(new PathString("/api"));
 app.UseRouting();
